7e8e27cda686d356497e06147920bb49248eccea
gigmep <gigmep33@gmail.com>
2025-08-07 22:45:52 +0300

    Update LottieHack.jsx

--- src/components/LottieHack.jsx
+++ src/components/LottieHack.jsx
@@ -4,14 +4,8 @@ import hackingAnim from '../animations/hacking.json';
 function LottieHack() {
   return (
     <div className="flex justify-center">
-      <Player
-        autoplay
-        loop
-        src={hackingAnim}
-        style={{ height: '200px', width: '200px' }}
-      />
+      <Player autoplay loop src={hackingAnim} style={{ height: '200px', width: '200px' }} />
     </div>
   );
 }
-
 export default LottieHack;
